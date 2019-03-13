0141d895ae3e52ca07ef4cf1ebe60bad4dee8bb8
MrParris <anthony.parris@usherbrooke.ca>
2019-03-13 16:51:09 -0400

    created and tested successfully waypointNode, ready to connect to real nodes

--- Robot_Software/securbot_pkg/scripts/splamNodeTest.py
+++ Robot_Software/securbot_pkg/scripts/splamNodeTest.py
@@ -2,19 +2,31 @@
 
 import rospy
 from rtabmap_ros.srv import SetGoal
+from rtabmap_ros.srv import SetGoalResponse
 
+#Handler called when request received from service client 
+def handleSplamNodeTest(req):
+    #Printing arguments of the request
+    print "Receiving node_id : %s"%(req.node_id) 
+    print "Receiving node_label : %s"%(req.node_label)
 
-def handleSplamNodeTestServer(req):
-    print "Receiving : %s"%(req)
-    return req
-def splamNodeTestServer():
-    rospy.init_node('splamNodeTestServer')
-    s = rospy.Service('splamNodeTestServer', SetGoal, handleSplamNodeTestServer)
+    #Creating a test response to send back to service client
+    resp = SetGoalResponse()   
+    return resp
+
+def splamNodeTest():
+    #Node name
+    rospy.init_node('splamNodeTest')
+
+    #Declaring that this node will provide a service as a server
+    s = rospy.Service('splamNodeTest', SetGoal, handleSplamNodeTest)
     print "SPLAM NODE TEST SERVER READY"
+    
+    #Starting node server 
     rospy.spin()
 
 if __name__ == "__main__":
-    splamNodeTestServer()
+    splamNodeTest()
 
 
 
