57b4a1b113a2db5a48e0c26fe4e4880ccbddc6db
MoronixProduct3 <dene2303@usherbrooke.ca>
2019-03-27 14:02:29 -0400

    Tool to create test video feeds by Edouard Legare

---
+++ scripts/VirtualCamSecurBot/scripts/startMap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Starting Virtual Camera with ffmpeg..."
+sudo modprobe v4l2loopback video_nr=3
+
+ffmpeg -loop 1 -re -i ../images/Map.png -f v4l2 -vcodec rawvideo -pix_fmt yuv420p /dev/video3
+
+sudo rmmod v4l2loopback
+echo "Virtual camera stopped..."
