42f73b45d9ff1f1645961809c6e4d26f0d3428e2
MoronixProduct3 <moronicproduct3@gmail.com>
2019-03-30 15:52:53 +0000

    SEC-347 Integrated VirtualCam into dev tools

--- scripts/VirtualCamSecurBot/scripts/startTest.sh
+++ scripts/VirtualCamSecurBot/scripts/startTest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting Virtual Camera with ffmpeg..."
-sudo modprobe v4l2loopback video_nr=3
+sudo modprobe v4l2loopback video_nr=3 card_label="virtual_camera"
 
 ffmpeg -loop 1 -re -i ../images/test.png -f v4l2 -vcodec rawvideo -pix_fmt yuv420p /dev/video3
 
