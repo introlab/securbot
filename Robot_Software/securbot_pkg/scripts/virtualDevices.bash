14330919e19473d790031ad88c811694c1490fcb
MoronixProduct3 <moronicproduct3@gmail.com>
2019-03-30 15:52:53 +0000

    SEC-347 Integrated VirtualCam into dev tools

--- Robot_Software/securbot_pkg/scripts/virtualDevices.bash
+++ Robot_Software/securbot_pkg/scripts/virtualDevices.bash
@@ -5,9 +5,10 @@
 # If lauched from ROS, ths script must be ran as root or using sudo through a user with the NOPASSWD setting
 
 if [[ $EUID -ne 0 ]]; then
-    sudo modprobe -r v4l2loopback
-    sudo modprobe v4l2loopback video_nr=1,2 card_label="virtual_kinect","virtual_map"
+    PREFIX="sudo"
 else
-    modprobe -r v4l2loopback
-    modprobe v4l2loopback video_nr=1,2 card_label="virtual_kinect","virtual_map"
+    unset PREFIX
 fi
+
+$PREFIX modprobe -r v4l2loopback
+$PREFIX modprobe v4l2loopback video_nr=1,2,3 card_label="virtual_kinect","virtual_map","fake_video"
