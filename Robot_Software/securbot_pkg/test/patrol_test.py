840b85e026fced7eb937506a511e2f2222625e71
MoronixProduct3 <dene2303@usherbrooke.ca>
2019-04-10 19:08:52 -0400

    SEC-351 tested transmission of first goal

--- Robot_Software/securbot_pkg/test/patrol_test.py
+++ Robot_Software/securbot_pkg/test/patrol_test.py
@@ -4,6 +4,7 @@ PKG = 'securbot_pkg'
 import sys, json, unittest, rospy, time
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
+from move_base_msgs.msg import MoveBaseActionGoal
 
 fakePatrol = '{ "patrol":[ {"x":1,"y":2,"yaw":3}, {"x":2,"y":1,"yaw":4}, {"x":3,"y":0,"yaw":3}, {"x":4,"y":-1,"yaw":4} ], "loop": false }'
 
@@ -16,13 +17,17 @@ class  PatrolTestSuite(unittest.TestCase):
     mapImageOutput = rospy.Publisher('/map_image/output_goal', PoseStamped, queue_size=20)
 
     conversionRequests = []
+    move_base_actions = []
 
     def mapImageCallBack(self, data):
         self.conversionRequests.append(data)
 
+    def moveBaseCallBack(self, data):
+        self.move_base_actions.append(data)
+
     # Send json string and verify that waypoints are being queried for conversion
     def test_patrol_exec(self):
-        moveBaseSub = rospy.Subscriber('/map_image/input_goal', PoseStamped, self.mapImageCallBack)
+        rospy.Subscriber('/map_image/input_goal', PoseStamped, self.mapImageCallBack)
         time.sleep(1)
         self.patrolPublisher.publish(fakePatrol)
         time.sleep(1)
@@ -31,6 +36,8 @@ class  PatrolTestSuite(unittest.TestCase):
     # Send conversion responses and make sure they got registered
     def test_waypoint_after_conversion(self):
         # Listen for movebase messages
+        rospy.Subscriber('/move_base/goal', MoveBaseActionGoal, self.moveBaseCallBack)
+        time.sleep(1)
 
         # Send conveterted waypoints
         for waypoint in self.conversionRequests:
@@ -38,7 +45,7 @@ class  PatrolTestSuite(unittest.TestCase):
         time.sleep(1)
 
         # Assert messages being sent to movebase
-        self.assertEquals(1,1,'hey')
+        self.assertEquals(len(self.move_base_actions), 1, 'Expecting move_base to receive a goal (got: '+str(len(self.move_base_actions))+')')
 
 
 if __name__ == '__main__':
