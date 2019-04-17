7d16a824b3a881226088b2064a685d81d5a761ba
MrParris <anthony.parris@usherbrooke.ca>
2019-04-17 11:27:05 -0400

    add last feature to send feedback toward electron node, add unit test

--- Robot_Software/securbot_pkg/test/patrol_test.py
+++ Robot_Software/securbot_pkg/test/patrol_test.py
@@ -6,7 +6,9 @@ from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
 from move_base_msgs.msg import MoveBaseActionGoal
 
-fakePatrol = '{ "patrol":[ {"x":1,"y":2,"yaw":3}, {"x":2,"y":1,"yaw":4}, {"x":3,"y":0,"yaw":3}, {"x":4,"y":-1,"yaw":4} ], "loop": false }'
+fakePatrol1 = '{ "patrol":[ {"x":1,"y":2,"yaw":3}, {"x":2,"y":1,"yaw":4}, {"x":3,"y":0,"yaw":3}, {"x":4,"y":-1,"yaw":4} ], "loop": false }'
+
+fakePatrol2 = '{ "patrol":[ {"x":1,"y":2,"yaw":3} ], "loop": false }'
 
 class  PatrolTestSuite(unittest.TestCase):
     rospy.init_node('patrol_test')
@@ -18,12 +20,19 @@ class  PatrolTestSuite(unittest.TestCase):
 
     conversionRequests = []
     move_base_actions = []
+    waypointsDoneStatus = []
+
+    INTERRUPT_TRUE_JSON = json.dumps({"interrupt":True})
 
     def mapImageCallBack(self, data):
         self.conversionRequests.append(data)
 
     def moveBaseCallBack(self, data):
         self.move_base_actions.append(data)
+
+    def waypointDoneCallback(self, data):
+        self.waypointsDoneStatus.append(json.loads(data)["waypointState"]) 
+        
 
     # Send json string and verify that waypoints are being queried for conversion
     def test_patrol_exec(self):
@@ -39,7 +48,7 @@ class  PatrolTestSuite(unittest.TestCase):
         rospy.Subscriber('/move_base/goal', MoveBaseActionGoal, self.moveBaseCallBack)
         time.sleep(3)
 
-        # Send conveterted waypoints
+        # Send converted waypoints
         for waypoint in self.conversionRequests:
             self.mapImageOutput.publish(waypoint)
         time.sleep(3)
@@ -47,7 +56,27 @@ class  PatrolTestSuite(unittest.TestCase):
         # Assert messages being sent to movebase
         self.assertEquals(len(self.move_base_actions), 1, 'Expecting move_base to receive a goal (got: '+str(len(self.move_base_actions))+')')
 
+    # Send 4 waypoints and expect they all have cancelled by the robot
+    def test_waypoint_is_cancelled(self):
+        rospy.Subscriber("datbot", String, waypointDoneCallback)       
+        time.sleep(3)
 
+        # Publish fake patrol 
+        patrolPublisher.publish(fakePatrol) 
+        time.sleep(3)
+        
+        # Publish interrupt
+        patrolCanceller.publish(INTERRUPT_TRUE_JSON) 
+        time.sleep(3)
+
+        # Assert waypoints have been interrupted
+        self.assertTrue(waypointsDoneStatus.len() > 0)
+
+        for status in waypointsDoneStatus:
+            self.assertNotEqual(status, "PENDING") 
+            self.assertNotEqual(status, "ACTIVE") 
+            self.assertNotEqual(status, "SUCCEEDED") 
+            
 if __name__ == '__main__':
     import rostest
     rostest.run(PKG, 'patrol_exec', PatrolTestSuite)
