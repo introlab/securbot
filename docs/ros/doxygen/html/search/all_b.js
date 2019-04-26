e37149bb398f5ad2caa638cd55457f4597b687a0
Edouard Legare <edouard.legare@usherbrooke.ca>
2019-04-26 16:26:16 -0400

    SEC-431 - updated main docs index and some configs

--- docs/ros/doxygen/html/search/all_b.js
+++ docs/ros/doxygen/html/search/all_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['securbot_20package',['SecurBot Package',['../md__home_edouard_securbot_ros_pkg_securbot_pkg_README.html',1,'']]],
+  ['securbot_20package',['SecurBot Package',['../index.html',1,'']]],
   ['sendgoaldonecallback',['sendGoalDoneCallback',['../namespacepatrol_executive.html#a2748fed61621678f4ea0b3d732482ae9',1,'patrolExecutive']]],
   ['setup',['setUp',['../classpatrol__test_1_1_patrol_test_suite.html#a8e2c5d056c4d289212f98752a2aab5fb',1,'patrol_test::PatrolTestSuite']]],
   ['startpatrolnavigation',['startPatrolNavigation',['../namespacepatrol_executive.html#a053fc2b8c4c45559e05ef75b84f3b570',1,'patrolExecutive']]],
--- docs/ros/doxygen/html/search/pages_0.js
+++ docs/ros/doxygen/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['securbot_20package',['SecurBot Package',['../md__home_edouard_securbot_ros_pkg_securbot_pkg_README.html',1,'']]]
+  ['securbot_20package',['SecurBot Package',['../index.html',1,'']]]
 ];
