070c30f4c420d65e7a118c167da6c1dc138dfe8b
Edouard Legare <edouard.legare@usherbrooke.ca>
2019-09-04 12:36:27 -0400

    Added Vuex package and file using @vue/cli

---
+++ frontend/src/store.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  // State = var/let calls
+  state: {
+
+  },
+  // Mutation is the same thing as a set call, needs to be synchronous
+  mutations: {
+
+  },
+  // Actions are methods that normally calls a mutation, but can do so asynchronously
+  actions: {
+
+  },
+});
