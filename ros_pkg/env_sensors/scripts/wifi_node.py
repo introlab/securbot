f405cd2c3598a99d3dbf08237d55fb1be1afdacf
AGuilbault <alexandre.p.guilbault@usherbrooke.ca>
2019-10-02 17:45:09 -0400

    SEC-496 Modified the wifi node to correctly send wifi levels to rtabmap

--- ros_pkg/env_sensors/scripts/wifi_node.py
+++ ros_pkg/env_sensors/scripts/wifi_node.py
@@ -3,8 +3,7 @@
 import rospy
 from pythonwifi.iwlibs import Wireless
 from rtabmap_ros.msg import UserData
-import numpy as np
-import cv2
+import struct
 
 def wifiPub():
 	# Create node
@@ -22,22 +21,25 @@ def wifiPub():
 		# Find signal level
 		stats = wifi.getStatistics()
 		dBm = stats[1].getSignallevel() - 256
-		
-		# Create maxtrix
-		data = np.zeros((1,2), dtype = "uint8")
-		data[0][0] = dBm
-		data[0][1] = stamp.secs
-		data2 = [np.uint8(stamp.secs),np.uint8(dBm)]
-		
+
 		# Create the User data
 		dataMsg = UserData()
 		dataMsg.header.frame_id = "Wifi_Level"
 		dataMsg.header.stamp = stamp
-		dataMsg.data = data2
+		
+		dataMsg.rows = 1
+		dataMsg.cols = 1
+		dataMsg.type = 6
+
+		# Add data to the message
+		dBm_ba = struct.pack('i',dBm)
+		dataMsg.data = dBm_ba
 		
 		# Publish
 		pub.publish(dataMsg)
 		rate.sleep()
+
+#def wifisub():
 
 
 if __name__ == '__main__':
