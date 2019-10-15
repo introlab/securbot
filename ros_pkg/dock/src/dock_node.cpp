1bf54c61cc849d18ddcf18afe024bea77d6874e6
MoronixProduct3 <moronicproduct3@gmail.com>
2019-10-15 02:42:00 -0400

    SEC-561 computing contact positions

--- ros_pkg/dock/src/dock_node.cpp
+++ ros_pkg/dock/src/dock_node.cpp
@@ -1,24 +1,40 @@
 #include "ros/ros.h"
 #include "apriltag_ros/AprilTagDetectionArray.h"
 #include "apriltag_ros/AprilTagDetection.h"
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
 
 #include <algorithm>
 
 int target_tag = -1;
 
-void DetectionArrayCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr& msg)
-{
 
-    for (int i; i < msg->detections.size(); ++i)
+bool findTagPose(
+        const apriltag_ros::AprilTagDetectionArray::ConstPtr& msg,
+        int tag,
+        geometry_msgs::PoseWithCovarianceStamped * out_pose)
+{
+    for (int i = 0; i < msg->detections.size(); ++i)
     {
         const apriltag_ros::AprilTagDetection & detection = msg->detections[i];
 
         if (std::count(detection.id.begin(), detection.id.end(), target_tag))
         {
-            ROS_INFO("Found suitable candidate");
-            return;
+            *out_pose = detection.pose;
+            return true;
         }
     }
+    return false;
+}
+
+void DetectionArrayCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr& msg)
+{
+    geometry_msgs::PoseWithCovarianceStamped targetPose;
+
+    if (! findTagPose(msg, target_tag, &targetPose))
+        return;
+
+    // Compute target position based of tag
+
 }
 
 int main (int argc, char **argv)
@@ -29,9 +45,10 @@ int main (int argc, char **argv)
 
     // Parameters
     nh.param("dock/tag_id", target_tag, 0);
+    ROS_INFO("Searching for tag #%d", target_tag);
 
     // Subscribed topics
-    ros::Subscriber sub = nh.subscribe("detect_array", 100, DetectionArrayCallback);
+    ros::Subscriber sub = nh.subscribe("detect_array", 0, DetectionArrayCallback);
 
     ros::spin();
 
