cae97b3ec709f086d9c27f4faed0a56b131894db
MoronixProduct3 <moronicproduct3@gmail.com>
2019-10-23 17:04:15 -0400

    SEC-561 guidance tests

--- ros_pkg/dock/src/guidance_node.cpp
+++ ros_pkg/dock/src/guidance_node.cpp
@@ -1,9 +1,9 @@
 #include "ros/ros.h"
-#include "tf2_ros/transform_listener.h"
 #include "nav_msgs/Path.h"
+#include "tf2_ros/transform_listener.h"
+#include <costmap_2d/costmap_2d_ros.h>
 #include <base_local_planner/trajectory_planner_ros.h>
 #include <string>
-#include <cmath>
 #include <mutex>
 
 
@@ -11,33 +11,45 @@ std::mutex g_goalMutex;
 tf2_ros::Buffer g_tfBuffer;
 
 
-
-void pathPlanCallback(const nav_msgs::Path::ConstPtr& msg)
+void GoalCallback(const nav_msgs::Path::ConstPtr& msg)
 {
+    std::scoped_lock(g_goalMutex);
 
+    ROS_INFO("Updating plan");
+
+}
+
+void OutputGuidance()
+{
+    std::scoped_lock(g_goalMutex);
 }
 
 
 int main (int argc, char **argv)
 {
-    ros::init(argc, argv, "planner");
+    ros::init(argc, argv, "guidance");
     ros::NodeHandle nh;
 
 
     // Initializing the transform buffer
     tf2_ros::TransformListener tfListener(g_tfBuffer);
 
+    // Init subscribers
+    ros::Subscriber goalSubscriber = nh.subscribe("approach_plan", 100, GoalCallback);
 
     // Init publisher
 
 
-    // Init subscibers
-
+    // Init local planner
+    costmap_2d::Costmap2DROS costmap("guidance_costmap", g_tfBuffer);
+    base_local_planner::TrajectoryPlannerROS localPlanner;
+    localPlanner.initialize("my_trajectory_planner", &g_tfBuffer, &costmap);
 
 
     ros::Rate rate(100.0);
     while(nh.ok())
     {
+        OutputGuidance();
 
         rate.sleep();
     }
