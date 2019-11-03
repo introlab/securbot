b2efe9c9d627751951405315506c8794ed15b183
Edouard Legare <edouard.legare@usherbrooke.ca>
2019-11-03 01:24:56 -0400

    SEC-568 Integrated dotenv package to the electron app, removed everything about fetching parameters and replaced them with global values from .env in ipc

--- ros_pkg/electron_webrtc/ipcInit.js
+++ ros_pkg/electron_webrtc/ipcInit.js
@@ -5,8 +5,12 @@
  */
 
 const { ipcRenderer } = require('electron');
+require('dotenv').config();
 
 process.once('loaded', () => {
   global.ipc = ipcRenderer;
+  global.webrtcServerURL = process.env.WEBRTC_SERVER_URL;
+  global.virtualDeviceNames = [process.env.VIRTUAL_DEVICE_CAMERA, process.env.VIRTUAL_DEVICE_MAP];
+  global.robotName = process.env.ROBOT_NAME;
   global.hello = 'hello';
 });
